4a0d827d2acca97e1dced951785d44b93f7a83a3
mychistous <mychistous@mts.ru>
2024-06-05 11:10:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/timer/test/MainActivity2.kt
@@ -0,0 +1,15 @@
+package com.example.timer.test
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        findViewById<Button>(R.id.go_back).setOnClickListener {
+            finish()
+        }
+    }
+}
