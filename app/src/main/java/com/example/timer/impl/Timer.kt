0af061ffa1a16c3ef31144a17222f8c2ef0a1d93
mychistous <mychistous@mts.ru>
2024-06-05 12:52:49 +0300

    solution chistousov

--- app/src/main/java/com/example/timer/impl/Timer.kt
+++ app/src/main/java/com/example/timer/impl/Timer.kt
@@ -1,7 +1,16 @@
 package com.example.timer.impl
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
+import kotlin.time.DurationUnit
 
 interface Timer {
     fun startTimer()
@@ -11,25 +20,63 @@ interface Timer {
     fun destroyTimer()
 }
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class TimerImpl(
-    val scope: CoroutineScope,
-    val providers: List<Provider> = listOf(
+    scope: CoroutineScope,
+    private val providers: List<Provider> = listOf(
         testProvider("firstProvider", 30.seconds),
         testProvider("secondProvider", 60.seconds),
         testProvider("thirdProvider", 10.seconds),
         testProvider("fourProvider", 44.seconds),
     )
 ) : Timer {
+
+    private val stateFlow = MutableStateFlow(State.STOP)
+
+    init {
+        scope.launch {
+            flow {
+                var spendSeconds = 0
+                stateFlow.flatMapLatest {  state ->
+                    if (state == State.STOP) {
+                        emptyFlow()
+                    } else {
+                        flow {
+                            while (isActive) {
+                                delay(1.seconds)
+                                emit(Unit)
+                            }
+                        }
+                    }
+                }.collect {
+                    spendSeconds += 1
+                    emit (spendSeconds)
+                }
+            }.collect { seconds ->
+                providers.filter {
+                    seconds % it.getSyncInterval().toInt(DurationUnit.SECONDS) == 0
+                }.forEach {
+                    it.syncTime(seconds.seconds)
+                }
+            }
+        }
+    }
+
+
     override fun startTimer() {
-        // TODO("Not yet implemented")
+        stateFlow.value = State.START
     }
 
     override fun stopTimer() {
-        // TODO("Not yet implemented")
+        stateFlow.value = State.STOP
     }
 
     override fun destroyTimer() {
-        // TODO("Not yet implemented")
+        stateFlow.value = State.STOP
+    }
+
+    enum class State {
+        START, STOP;
     }
 
 }
