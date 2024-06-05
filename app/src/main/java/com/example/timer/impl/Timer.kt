56979b5e003bfbe62e54b11dce1b49b28ef4d6ff
mychistous <mychistous@mts.ru>
2024-06-05 13:45:33 +0300

    solution chistousov

--- app/src/main/java/com/example/timer/impl/Timer.kt
+++ app/src/main/java/com/example/timer/impl/Timer.kt
@@ -2,13 +2,16 @@ package com.example.timer.impl
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import java.util.Date
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.DurationUnit
 
@@ -33,31 +36,71 @@ class TimerImpl(
 
     private val stateFlow = MutableStateFlow(State.STOP)
 
+    private val timerJob: Job
+
     init {
-        scope.launch {
+        timerJob = scope.launch {
             flow {
                 var spendSeconds = 0
-                stateFlow.flatMapLatest {  state ->
+                var lastStartTime: Long? = null
+                stateFlow.flatMapLatest { state ->
                     if (state == State.STOP) {
-                        emptyFlow()
-                    } else {
                         flow {
-                            while (isActive) {
-                                delay(1.seconds)
-                                emit(Unit)
+                            lastStartTime?.let { startTime ->
+                                val stopTime = Date().time / 1000
+                                val duration = (stopTime - startTime).seconds
+                                emit(TimerAction.Stop(duration))
                             }
                         }
+                    } else {
+                        flow<TimerAction> {
+                            while (isActive) {
+                                delay(1.seconds)
+                                emit(TimerAction.Handle)
+                            }
+                        }.onStart {
+                            val startTime = Date().time / 1000
+                            lastStartTime = startTime
+                            emit(TimerAction.Start(startTime.seconds))
+                        }
                     }
-                }.collect {
-                    spendSeconds += 1
-                    emit (spendSeconds)
+                }.collect { action ->
+                    when (action) {
+                        is TimerAction.Handle -> {
+                            spendSeconds += 1
+                            emit(TimerEvent.Handle(spendSeconds.seconds))
+                        }
+
+                        is TimerAction.Start -> {
+                            emit(TimerEvent.Start(action.duration))
+                        }
+
+                        is TimerAction.Stop -> {
+                            emit(TimerEvent.Stop(action.duration))
+                        }
+                    }
                 }
-            }.collect { seconds ->
-                providers.filter {
-                    seconds % it.getSyncInterval().toInt(DurationUnit.SECONDS) == 0
-                }.forEach {
-                    it.syncTime(seconds.seconds)
+            }.collect { event ->
+                when (event) {
+                    is TimerEvent.Handle -> {
+                        providers.filter {
+                            event.duration.toInt(DurationUnit.SECONDS) % it.getSyncInterval().toInt(DurationUnit.SECONDS) == 0
+                        }.forEach {
+                            launch { it.syncTime(event.duration) }
+                        }
+                    }
+                    is TimerEvent.Start -> {
+                        providers.forEach {
+                            launch { it.onTimerStart(event.duration) }
+                        }
+                    }
+                    is TimerEvent.Stop -> {
+                        providers.forEach {
+                            launch { it.onTimerStop(event.duration) }
+                        }
+                    }
                 }
+
             }
         }
     }
@@ -73,10 +116,22 @@ class TimerImpl(
 
     override fun destroyTimer() {
         stateFlow.value = State.STOP
+        timerJob.cancel()
     }
 
-    enum class State {
+    private enum class State {
         START, STOP;
     }
 
+    private sealed interface TimerAction {
+        data class Start(val duration: Duration) : TimerAction
+        data class Stop(val duration: Duration) : TimerAction
+        data object Handle : TimerAction
+    }
+
+    private sealed interface TimerEvent {
+        data class Start(val duration: Duration) : TimerEvent
+        data class Stop(val duration: Duration) : TimerEvent
+        data class Handle(val duration: Duration) : TimerEvent
+    }
 }
