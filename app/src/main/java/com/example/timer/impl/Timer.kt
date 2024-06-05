82290c20df36ff9a8fb52af7bc5ae313b9e8d76c
mychistous <mychistous@mts.ru>
2024-06-05 16:45:58 +0300

    solution nikolay

--- app/src/main/java/com/example/timer/impl/Timer.kt
+++ app/src/main/java/com/example/timer/impl/Timer.kt
@@ -1,8 +1,14 @@
 package com.example.timer.impl
 
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
 
+const val TIMER_PERIOD = 1000L
+const val TIMER_TICK = 1
 interface Timer {
     fun startTimer()
 
@@ -10,26 +16,57 @@ interface Timer {
 
     fun destroyTimer()
 }
-
+@RequiresApi(Build.VERSION_CODES.O)
 class TimerImpl(
-    val scope: CoroutineScope,
-    val providers: List<Provider> = listOf(
-        testProvider("firstProvider", 30.seconds),
-        testProvider("secondProvider", 60.seconds),
-        testProvider("thirdProvider", 10.seconds),
-        testProvider("fourProvider", 44.seconds),
+    private val scope: CoroutineScope,
+    private val providers: List<Provider> = listOf(
+        testProvider("TimerTag firstProvider", 30.seconds),
+        testProvider("TimerTag secondProvider", 60.seconds),
+        testProvider("TimerTag thirdProvider", 10.seconds),
+        testProvider("TimerTag fourProvider", 44.seconds),
     )
 ) : Timer {
+
+
+    private var timer: java.util.Timer? = null
+    private var currentTimeSpent = 0L
+    private var lastStartTime = 0L
     override fun startTimer() {
-        // TODO("Not yet implemented")
+        Log.d("TimerTag","Instance = $this")
+        lastStartTime = currentTimeSpent
+        scope.launch {
+            providers.forEach { it.onTimerStart(currentTimeSpent.seconds) }
+            timer = kotlin.concurrent.timer(
+                period = TIMER_PERIOD,
+            ) {
+                currentTimeSpent += TIMER_TICK
+                notifyProvider()
+                Log.d("TimerTag","timeTick = 1 currentTimeSpent = $currentTimeSpent")
+            }
+        }
     }
 
     override fun stopTimer() {
-        // TODO("Not yet implemented")
+        scope.launch {
+            val spentTime = (currentTimeSpent - lastStartTime).seconds
+            providers.forEach { it.onTimerStop(spentTime) }
+        }
+        timer?.cancel()
     }
 
     override fun destroyTimer() {
-        // TODO("Not yet implemented")
+        timer?.cancel()
+        timer = null
     }
 
+    private fun notifyProvider() {
+        scope.launch {
+            providers.forEach {
+                val syncInterval = it.getSyncInterval()
+                if(currentTimeSpent % syncInterval.inWholeSeconds == 0L ) {
+                    it.syncTime(currentTimeSpent.seconds)
+                }
+            }
+        }
+    }
 }
