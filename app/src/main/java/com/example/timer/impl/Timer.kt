b88d7a0b8671cacc9a26adbf306ab1a6811def0d
mychistous <mychistous@mts.ru>
2024-06-05 16:49:05 +0300

    solution roma

--- app/src/main/java/com/example/timer/impl/Timer.kt
+++ app/src/main/java/com/example/timer/impl/Timer.kt
@@ -1,6 +1,8 @@
 package com.example.timer.impl
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.seconds
 
 interface Timer {
@@ -20,16 +22,43 @@ class TimerImpl(
         testProvider("fourProvider", 44.seconds),
     )
 ) : Timer {
+    private val isTimerPaused = MutableStateFlow(false)
+    private val startTime = MutableStateFlow((System.currentTimeMillis() / 1000).seconds)
+    private val leaveTime = MutableStateFlow((System.currentTimeMillis() / 1000).seconds)
     override fun startTimer() {
-        // TODO("Not yet implemented")
+        if (isTimerPaused.value.not()) {
+            startTime.value = (System.currentTimeMillis() / 1000).seconds
+            scope.launch {
+                providers.forEach { provider ->
+                    provider.onTimerStart(startTime.value)
+                }
+            }
+        } else {
+            scope.launch {
+                providers.forEach { provider ->
+                    if (leaveTime.value.inWholeSeconds - startTime.value.inWholeSeconds < provider.getSyncInterval().inWholeSeconds) {
+                        provider.onTimerStart(startTime.value)
+                    } else {
+                        provider.onTimerStart((System.currentTimeMillis() / 1000).seconds)
+                    }
+                }
+            }
+            isTimerPaused.value = false
+        }
     }
 
     override fun stopTimer() {
-        // TODO("Not yet implemented")
+        isTimerPaused.value = true
+        val stopTime = (System.currentTimeMillis() / 1000).seconds
+        leaveTime.value = stopTime
+        scope.launch {
+            providers.forEach { provider ->
+                provider.onTimerStop(stopTime - startTime.value)
+            }
+        }
     }
 
     override fun destroyTimer() {
-        // TODO("Not yet implemented")
     }
 
 }
